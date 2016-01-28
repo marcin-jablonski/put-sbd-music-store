000b9ffb096db1a9564a743d6bfbfce5dffa58c9
Marcin Jabłoński <jablon1994@gmail.com>
2016-01-28 20:07:18 +0100

    Retrieve user data from DB

--- src/musicstore/logic/AccountInfoRetriever.java
+++ src/musicstore/logic/AccountInfoRetriever.java
@@ -5,6 +5,11 @@
  */
 package musicstore.logic;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import musicstore.datastructures.AccountInfo;
 
 /**
@@ -14,15 +19,35 @@ import musicstore.datastructures.AccountInfo;
 public class AccountInfoRetriever {
     public static AccountInfo GetAccountInfo(int userID)
     {
-        AccountInfo mockInfo = new AccountInfo();
+        AccountInfo info = new AccountInfo();
+        Connection conn = DBConnector.Connect();
+        try{
+            PreparedStatement stmt = conn.prepareStatement("SELECT FIRSTNAME, LASTNAME FROM CLIENT WHERE ID = ?");
+            stmt.setInt(1, userID);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                info.setFirstName(rs.getString(1));
+                info.setLastName(rs.getString(2));
+            }
+            rs.close();
+            stmt.close();
+            
+            stmt = conn.prepareStatement("SELECT PRICE FROM CARTITEM JOIN GUITAR ON CARTITEM.GUITAR = GUITAR.ID WHERE CARTITEM.CLIENT = ?");
+            stmt.setInt(1, userID);
+            rs = stmt.executeQuery();
+            info.setCartState(0);
+            info.setTotalPrice((float) 0.0);
+            while(rs.next()){
+                info.setCartState(info.getCartState() + 1);
+                info.setTotalPrice(info.getTotalPrice() + rs.getFloat(1));
+            }
+        } catch(Exception e) {
+            Logger.getLogger(AccountInfoRetriever.class.getName()).log(Level.SEVERE, null, e);
+        }
         
-        mockInfo.setCartState(3);
-        mockInfo.setFirstName("Marcin");
-        mockInfo.setLastName("Jabłoński");
-        mockInfo.setId(1);
-        mockInfo.setTotalPrice((float) 30.99);
-        mockInfo.setAvatarPath("src/resources/avatars/sample.jpg");
-        
-        return mockInfo;
+        DBConnector.Disconnect(conn);
+        info.setAvatarPath("src/resources/avatars/sample.jpg");
+        info.setId(userID);
+        return info;
     }
 }
